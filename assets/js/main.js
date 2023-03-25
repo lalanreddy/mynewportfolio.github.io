bcb5bb7b219d162d1738ebc4d3d26c6b3f9794ef
lalan reddy <lalanreddy1605@gmail.com>
2023-03-25 13:38:57 -0500

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+function sendEmail(event) {
+    event.preventDefault();
+
+    // Change these values to match your own email configuration
+    const email = "lalanreddy1605@example.com";
+    const subject = "New message from your website";
+    const name = document.getElementsByName("name")[0].value;
+    const senderEmail = document.getElementsByName("email")[0].value;
+    const message = document.getElementsByName("message")[0].value;
+
+    // Format the email body
+    const body = "Name: " + name + "\nEmail: " + senderEmail + "\n\n" + message;
+
+    // Launch the email client with a pre-filled email
+    window.open("mailto:" + email + "?subject=" + subject + "&body=" + body);
+}
+
+const submitButton = document.querySelector('.contact__button');
+submitButton.addEventListener('click', sendEmail);
+
